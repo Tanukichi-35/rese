a5f09f1c67c94a63bfa089beeeb50a73448f00d5
Tanukichi-35 <yo-fu@outlook.jp>
2024-03-07 20:43:23 +0900

    DBテーブル作成

---
+++ src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => 'Manager',
+            'email' => 'manager@store.jp',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'level' => '2'
+        ];
+        DB::table('users')->insert($param);
+    }
+}
